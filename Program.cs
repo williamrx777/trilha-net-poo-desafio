e0c2d5433e34cff581a9c06fb95109fbbfd82788
william rodrigues <38387964+williamrx777@users.noreply.github.com>
2025-08-02 11:49:53 -0300

    Correção de codigo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Testando Nokia
+Nokia nokia = new Nokia(numero: "123456", modelo: "Model 1", imei: "111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+// Testando iPhone
+Iphone iphone = new Iphone(numero: "4987555", modelo: "Model 2", imei: "222222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
+
